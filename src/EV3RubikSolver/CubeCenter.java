6fa5fdec40ba7d3e14878ad7d12c69c382da4ca9
TIMYY <timyy@126.com>
2016-01-06 23:19:03 +0800

    Cube ok. number voice.

--- src/EV3RubikSolver/CubeCenter.java
+++ src/EV3RubikSolver/CubeCenter.java
@@ -13,40 +13,44 @@ public class CubeCenter
 
     public void RotateBottom(boolean colockwise)
     {
-        if (colockwise)
-        {
-            String n = CenterColor[5];
-            CenterColor[5] = CenterColor[1];
-            CenterColor[1] = CenterColor[4];
-            CenterColor[4] = CenterColor[3];
-            CenterColor[3] = n;
-        }
-        else
-        {
-            String n = CenterColor[5];
-            CenterColor[5] = CenterColor[3];
-            CenterColor[3] = CenterColor[4];
-            CenterColor[4] = CenterColor[1];
-            CenterColor[1] = n;
-        }
-    }
-
-    public void RotatePaw()
-    {
-        //Only can move forward
-        String n = CenterColor[0];
-        CenterColor[0] = CenterColor[3];
-        CenterColor[3] = CenterColor[2];
-        CenterColor[2] = CenterColor[1];
-        CenterColor[1] = n;
-    }
+        if (colockwise)  
+        {  
+            String n = CenterColor[5];  
+            CenterColor[5] = CenterColor[1];  
+            CenterColor[1] = CenterColor[4];  
+            CenterColor[4] = CenterColor[3];  
+            CenterColor[3] = n;  
+        }  
+        else  
+        {  
+            String n = CenterColor[5];  
+            CenterColor[5] = CenterColor[3];  
+            CenterColor[3] = CenterColor[4];  
+            CenterColor[4] = CenterColor[1];  
+            CenterColor[1] = n;  
+        }  
+    }  
+  
+    public void RotatePaw()  
+    {  
+        //Only can move forward  
+        String n = CenterColor[0];  
+        CenterColor[0] = CenterColor[3];  
+        CenterColor[3] = CenterColor[2];  
+        CenterColor[2] = CenterColor[1];  
+        CenterColor[1] = n;  
+    }  
 
     public int FindCenter(String position)
     {
         int center = -1;
+        // System.out.print("FindCenter:" + position);
         for (int i = 0; i < 6; i++)
         {
-            if (CenterColor[i] == position) center = i;
+            // if (CenterColor[i] == position) center = i; 
+        	//BUG: 20151231 java 不能这么写，和常量比还行，变量不可以。java比的是地址。这是C#的写法
+        	if(position.equals(CenterColor[i])) center =i;
+        	// System.out.print( i + "!" +CenterColor[i] + "!" + center + "@");
         }
         return center;
     }
